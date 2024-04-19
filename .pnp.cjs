6a55de21e0ea0e0854c52fcddffb8a63151683b0
saadazghour <saad.azghour@gmail.com>
2024-04-19 15:50:30 +0100

    feat: install Excalidraw && Fal.ai packages for real-time AI drawing integration
    - Added @excalidraw/excalidraw for the drawing interface
    - Added @fal-ai/serverless-client && @fal-ai/serverless-proxy for AI processing backend integration

---
+++ .pnp.cjs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cfe541d32fc05ed22c80452f02bdee2c7a7fb83a0aa1492ec6c5170fe0c614d7
+size 785070
---
+++ .yarn/unplugged/node-gyp-npm-10.1.0-bdea7d2ece/node_modules/node-gyp/gyp/pylib/gyp/xcodeproj_file.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9fce2726e3940724ef8e7d7bb68c51f4d828599f5b6aa32d1ded29c8871c87ad
+size 135641
