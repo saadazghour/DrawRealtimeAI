393e609384219a66869251a31c9535c4c0ced946
saadazghour <saad.azghour@gmail.com>
2024-04-25 17:17:44 +0100

    refactor FAL API && improve image handling!
    - Update the proxy configuration for better API key protection.
    - Change the initial input state to better align with new use cases.
    - Replace the blob handling logic to use FileReader for converting blobs to data URLs.
    - Adjust getDataURL function to ensure proper image dimensions are set.
    - Enhance Excalidraw onChange handler to optimize for changes in the canvas state.

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,11 @@ import {
 import { useState } from "react";
 import * as fal from "@fal-ai/serverless-client";
 
+// ***** Setup the proxy
+
+// The proxy will protect your API Key and prevent it from being exposed to the client.Usually app implementation have to handle that integration themselves, but in order to make the integration as smooth as possible, we provide a drop -in proxy implementation that can be integrated with the App Router.
+
+// The proxy will also handle the request and response.
 fal.config({
   proxyUrl: "/api/fal/proxy",
 });
@@ -22,9 +27,8 @@ const baseArgs = {
 };
 
 export default function Home() {
-  const [input, setInput] = useState(
-    "Photo of a european medieval 40 year old queen, silver hair, highly detailed face, detailed eyes, head shot, intricate crown, age spots, wrinkles"
-  );
+  const [input, setInput] = useState("Anthropomorphic cat dressed as a pilot");
+
   const [image, setImage] = useState(null); // Image will be updated as the Image come back from the fal API's
 
   const [syncData, setSyncData] = useState<any>(null);
@@ -34,9 +38,11 @@ export default function Home() {
   const { send } = fal.realtime.connect("110602490-sdxl-turbo-realtime", {
     connectionKey: "110602490-lora-realtime",
     onResult: (result) => {
-      console.log("result send!", result);
       if (result.error) return;
       setImage(result.images[0].url);
+    },
+    onError: (error) => {
+      console.error(error);
     },
   });
 
@@ -45,35 +51,87 @@ export default function Home() {
 
     if (!elements || !elements.length) return;
 
+    // The Blob object represents a blob, which is a file-like object of immutable, raw data; they can be read as text or binary data, or converted into a ReadableStream so its methods can be used for processing the data.
+    // *******
+    // Blobs can represent data that isn't necessarily in a JavaScript-native format. The File interface is based on Blob, inheriting blob functionality and expanding it to support files on the user's system.
+
     const blob = await exportToBlob({
       elements,
       exportPadding: 10,
       appState,
       quality: 1,
       files: excalidrawAPI.getFiles(),
-      getDimensions: (width, height) => ({ width, height }),
+      getDimensions: () => {
+        return { width: 450, height: 450 };
+      },
       mimeType: "image/png",
     });
 
-    return dataURL;
+    return await new Promise((resolve) => {
+      let reader = new FileReader();
+      reader.onload = resolve;
+
+      reader.readAsDataURL(blob);
+    }).then((e: any) => e.target.result);
   }
 
-  // const result = await fal.subscribe("110602490-lora", {
-  //   input: {
-  //     prompt,
-  //     model_name: "stabilityai/stable-diffusion-xl-base-1.0",
-  //     image_size: "square_hd",
-  //   },
-  //   pollInterval: 5000,
-  //   logs: true,
-  //   onQueueUpdate(update) {
-  //     console.log("queue update", update);
-  //   },
-  // });
-
-  // const imageUrl = result.images[0].url;
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col p-24">
+      <p className="text-xl mb-2">FAL SDXL</p>
+      <input
+        type="text"
+        className="border border-black rounded-md p-2 w-full mb-2"
+        value={input}
+        onChange={async (e) => {
+          const { value } = e.target;
+          setInput(value);
+          // fetch the new Data
+          let dataUrl = await getDataURL();
+          // then send the update to the fal API
+          send({
+            ...baseArgs,
+            prompt: value,
+            image_url: dataUrl,
+          });
+        }}
+      />
+
+      <div className="flex">
+        <div className="w-[636px] h-[570px]">
+          <Excalidraw
+            excalidrawAPI={(api) => setExcalidrawAPI(api)}
+            // This callback is triggered whenever the component updates due to any change. This callback will receive the excalidraw elements and the current app state.
+            onChange={async (elements, appState) => {
+              const newSyncData = serializeAsJSON(
+                elements,
+                appState,
+                excalidrawAPI.getFiles(),
+                "local"
+              );
+
+              if (newSyncData !== syncData) {
+                setAppState(appState);
+                setSyncData(newSyncData);
+                let dataURL = await getDataURL(appState);
+
+                send({
+                  ...baseArgs,
+                  prompt: input,
+                  image_url: dataURL,
+                });
+              }
+            }}
+          />
+        </div>
+        {image && (
+          <Image
+            src={image}
+            alt="Generated Fal Image"
+            width={550}
+            height={550}
+          />
+        )}
+      </div>
+    </main>
   );
 }
