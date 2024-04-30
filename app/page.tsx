339b504c432446acca2228c0c815c3b3f2d223ae
saadazghour <saad.azghour@gmail.com>
2024-04-30 17:23:18 +0100

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,10 @@ if (typeof window !== "undefined") {
   (window as any).EXCALIDRAW_ASSET_PATH = "/excalidraw/dist";
 }
 
-import Image from "next/image";
-import dynamic from "next/dynamic";
-
-// Since Excalidraw doesn't support server side rendering so it should be rendered only on client. The way to achieve this in next.js is using next.js dynamic import.
-
-const Excalidraw = dynamic(
-  async () => (await import("@excalidraw/excalidraw")).Excalidraw,
-  {
-    ssr: false, // This tells Next.js to only import this component on the client-side
-  }
-);
-
-import {
-  exportToBlob, // Export our canvas to an image
-  serializeAsJSON, // Serialize our canvas to JSON. To only update when canvas it's change
-} from "@excalidraw/excalidraw";
-
 import { useState } from "react";
+import Image from "next/image";
+import { DrawingCanvas } from "~@/components/DrawingCanvas";
 import * as fal from "@fal-ai/serverless-client";
-
-import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
-import { AppState } from "@excalidraw/excalidraw/types/types";
 
 // ***** Setup the proxy
 
@@ -46,52 +28,23 @@ const baseArgs = {
 
 export default function Home() {
   const [input, setInput] = useState("Anthropomorphic cat dressed as a pilot");
-
   const [image, setImage] = useState(null); // Image will be updated as the Image come back from the fal API's
 
-  const [syncData, setSyncData] = useState<any>(null);
-  const [excalidrawAPI, setExcalidrawAPI] = useState<any>(null);
-  const [_appState, setAppState] = useState<any>(null);
+  const [canvasImageData, setCanvasImageData] = useState("");
 
   const { send } = fal.realtime.connect("110602490-sdxl-turbo-realtime", {
     connectionKey: "110602490-lora-realtime",
-    onResult: (result) => {
+    onResult: (result: any) => {
       if (result.error) return;
-      setImage(result.images[0].url);
+
+      if (result.images && result.images[0]) {
+        setImage(result.images[0].url);
+      }
     },
-    onError: (error) => {
+    onError: (error: any) => {
       console.error(error);
     },
   });
-
-  async function getDataURL(appState = _appState) {
-    const elements = excalidrawAPI.getSceneElements();
-
-    if (!elements || !elements.length) return;
-
-    // The Blob object represents a blob, which is a file-like object of immutable, raw data; they can be read as text or binary data, or converted into a ReadableStream so its methods can be used for processing the data.
-    // *******
-    // Blobs can represent data that isn't necessarily in a JavaScript-native format. The File interface is based on Blob, inheriting blob functionality and expanding it to support files on the user's system.
-
-    const blob = await exportToBlob({
-      elements,
-      exportPadding: 10,
-      appState,
-      quality: 1,
-      files: excalidrawAPI.getFiles(),
-      getDimensions: () => {
-        return { width: 450, height: 450 };
-      },
-      mimeType: "image/png",
-    });
-
-    return await new Promise((resolve) => {
-      let reader = new FileReader();
-      reader.onload = resolve;
-
-      reader.readAsDataURL(blob);
-    }).then((e: any) => e.target.result);
-  }
 
   return (
     <main className="flex min-h-screen flex-col p-24">
@@ -103,48 +56,30 @@ export default function Home() {
         onChange={async (e) => {
           const { value } = e.target;
           setInput(value);
-          // fetch the new Data
-          let dataUrl = await getDataURL();
+
           // then send the update to the fal API
           send({
             ...baseArgs,
             prompt: value,
-            image_url: dataUrl,
+            image_url: canvasImageData,
           });
         }}
       />
 
       <div className="flex">
-        <div className="w-[636px] h-[570px]">
-          <Excalidraw
-            autoFocus={true}
-            excalidrawAPI={(api: any) => setExcalidrawAPI(api)}
-            // This callback is triggered wh  enever the component updates due to any change. This callback will receive the excalidraw elements and the current app state.
-            onChange={async (
-              elements: readonly ExcalidrawElement[],
-              appState: Partial<AppState>
-            ) => {
-              const newSyncData = serializeAsJSON(
-                elements,
-                appState,
-                excalidrawAPI.getFiles(),
-                "local"
-              );
+        <DrawingCanvas
+          onCanvasChange={(canvasEvent) => {
+            const { imageData } = canvasEvent;
+            setCanvasImageData(imageData);
 
-              if (newSyncData !== syncData) {
-                setAppState(appState);
-                setSyncData(newSyncData);
-                let dataURL = await getDataURL(appState);
+            send({
+              ...baseArgs,
+              prompt: input,
+              image_url: imageData,
+            });
+          }}
+        />
 
-                send({
-                  ...baseArgs,
-                  prompt: input,
-                  image_url: dataURL,
-                });
-              }
-            }}
-          />
-        </div>
         {image && (
           <Image
             src={image}
